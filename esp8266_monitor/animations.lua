86f7c8d0ac3b58a9f44bbc25ce378bff06172d55
Nathan Miller <nmiller014@gmail.com>
2017-03-26 19:25:56 -0500

    Refactored the animations module a lot

--- esp8266_monitor/animations.lua
+++ esp8266_monitor/animations.lua
@@ -1,50 +1,91 @@
 local M = {
-  led_count = 8,
-  tick_count = 0,
+  animations = {},
 }
 
-function M.draw()
-  if M.animation then
-    local led_data = M.animation.draw(M.led_count, M.tick_count)
+M.animations.none = function(led_count)
+  return string.char(0,0,0):rep(led_count), 0
+end
+
+M.animations.running = function(led_count, tick_count)
+  local led_data = ''
+  local position = tick_count % led_count
+  local direction = math.floor(tick_count / led_count) % 2
+  if direction == 1 then
+    position = (led_count - position) + 1
+  end
+
+  for i=1,led_count,1 do
+      local val = 0
+      if math.abs(position - i) == 0 then
+          val = 255
+      elseif math.abs(position - i) == 1 then
+          val = 75
+      elseif math.abs(position - i) == 2 then
+          val = 10
+      end
+      led_data = led_data .. string.char(0,0,val)
+  end
+
+  return led_data, 75
+end
+
+M.animations.success = function(led_count, tick_count)
+  local led_data = ''
+  local value = tick_count % 255
+  local direction = math.floor(tick_count / 255) % 2
+  if direction == 1 then
+    value = (255 - value) + 1
+  end
+
+  led_data = led_data .. string.char(val,0,0):rep(led_count)
+
+  local next_timeout = 0
+  
+  if (tick_count == 255) then
+    next_timeout = 1
+  elseif tick_count > 150 and tick_count < 255 then
+    next_timeout = 4
+  elseif ((tick_count > 125) and (tick_count < 151)) then
+    next_timeout = 5
+  elseif ((tick_count > 100) and (tick_count < 126)) then
+    next_timeout = 7
+  elseif ((tick_count > 75) and (tick_count < 101)) then
+    next_timeout = 10
+  elseif ((tick_count > 50) and (tick_count < 76)) then
+    next_timeout = 14
+  elseif ((tick_count > 25) and (tick_count < 51)) then
+    next_timeout = 18
+  elseif (tick_count < 26 and tick_count > 0) then
+    next_timeout = 19
+  elseif (tick_count == 0) then
+    next_timeout = 30
+  end
+
+  return led_data, next_timeout
+end
+
+function M.tick()
+  if M.draw then
+    local led_data, delay_ms = M.draw(M.led_count, M.tick_count)
     ws2812.write(led_data)
     M.tick_count = M.tick_count + 1
+    if delay_ms > 0 then
+      M.timer:alarm(delay_ms, tmr.ALARM_SINGLE, M.tick)
+    end
   end
 end
 
-M.animations = {}
+function M:set_animation(name)
+  self.draw = self.animations[name]
+  self.tick()
+end
 
-M.animations.running = {
-  period_ms = 100,
-  draw = function(led_count, tick_count)
-    local led_data = ''
-    local position = (tick_count % led_count) + 1
-    local direction = math.floor(tick_count / led_count) % 2
-    if direction == 1 then
-      position = (led_count - position) + 1
-    end
-
-    for i=1,led_count,1 do
-        local val = 0
-        if math.abs(position - i) == 0 then
-            val = 255
-        elseif math.abs(position - i) == 1 then
-            val = 75
-        elseif math.abs(position - i) == 2 then
-            val = 10
-        end
-        led_data = led_data .. string.char(0,0,val)
-    end
-
-    return led_data
-  end,
-}
-
-function M.set_animation(name)
+function M:init(led_count)
+  self.tick_count = 0
+  self.timer = tmr.create()
+  self.led_count = led_count
   ws2812.init(ws2812.MODE_SINGLE)
-  ws2812.write(string.char(0,0,0):rep(M.led_count))
-  M.animation = M.animations[name]
-  M.timer = M.timer or tmr.create()
-  M.timer:alarm(M.animation.period_ms, tmr.ALARM_AUTO, M.draw)
+  M:set_animation("none")
 end
 
 return M
