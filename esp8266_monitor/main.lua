eec81c03ce07b36897a95ce417bd1b31df4ce260
Nathan Miller <nmiller014@gmail.com>
2017-03-27 21:32:14 -0500

    Started adding more code to main.lua

--- esp8266_monitor/main.lua
+++ esp8266_monitor/main.lua
@@ -1,12 +1,35 @@
+local enduser_setup = require("enduser_setup")
 local animations = require("animations")
 local ui = require("ui")
 
+local function start()
+  -- TODO start bamboo ping here
+end
+
 local function tap_cb(when)
-  print("Tapped at ", when)
+  print("Button tapped")
+  enduser_setup.start()
 end
 
 local function hold_cb(when)
-  print("Held at ", when)
+  print("Button held")
+  print("Disconnecting from wifi")
+  wifi.sta.disconnect()
+end
+
+local wifi_status = wifi.sta.status()
+print("Wifi status:", wifi_status)
+
+if wifi_status ~= 5 then
+  -- not connected to wifi, start setup portal
+  enduser_setup.start(
+    function()
+      print("Connected to wifi as:" .. wifi.sta.getip())
+    end,
+    function(err, str)
+      print("enduser_setup: Err #" .. err .. ": " .. str)
+    end
+  )
 end
 
 animations:init(8)
