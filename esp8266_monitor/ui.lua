5552a3a829924ae4fb5ec8a0c24ce438a22fb7e7
Nathan Miller <nmiller014@gmail.com>
2017-03-27 21:21:45 -0500

    Got basic ui module working

--- esp8266_monitor/ui.lua
+++ esp8266_monitor/ui.lua
@@ -1,25 +1,34 @@
-local M = {}
+local M = {
+  held = false,
+  tap_expired = false,
+}
 
-function M.interrupt_callback(level, when)
-  print(level, when)
-  
-  if level == 0 then
-    hold_timer:alarm(150, tmr.ALARM_SINGLE, function()
-      hold_callback(when)
-    end)
-  else
-
-    hold_timer:unregister()
-
-  end
-end
-
-function M:init(pin, hold_callback, tap_callback)
+function M:init(pin, hold_time, tap_time, hold_callback, tap_callback)
   local hold_timer = tmr.create()
   local tap_timer = tmr.create()
 
   gpio.mode(pin, gpio.INT)
   gpio.trig(pin, "both", function(level, when)
+    if level == 0 then
+      hold_timer:alarm(hold_time, tmr.ALARM_SINGLE, function()
+        M.held = true
+        hold_callback(when)
+      end)
+
+      tap_timer:alarm(tap_time, tmr.ALARM_SINGLE, function()
+        M.tap_expired = true
+      end)
+    else
+      hold_timer:unregister()
+      tap_timer:unregister()
+      
+      if not M.held and not M.tap_expired then
+        tap_callback(when)
+      end
+
+      M.held = false
+      M.tap_expired = false
+    end
   end)
 end
 
