510a8547f0ddb25f287b43c64df805f7fc47922c
Nathan Miller <nmiller014@gmail.com>
2017-03-27 22:12:21 -0500

    Cleaned up main a lot and added minute timer

--- esp8266_monitor/main.lua
+++ esp8266_monitor/main.lua
@@ -1,44 +1,57 @@
-local enduser_setup = require("enduser_setup")
 local animations = require("animations")
-local ui = require("ui")
+local enduser_setup = require("enduser_setup")
+local button = require("button")
+local bamboo = require("bamboo")
 
-local function start()
-  print("Starting bamboo ping timer")
-  -- TODO start bamboo ping here
-end
+local setup_started = false
 
 local function setup()
   -- not connected to wifi, start setup portal
-  enduser_setup.start(
-    function()
-      print("Connected to wifi")
-      start()
-    end,
-    function(err, str)
-      print("enduser_setup: Err #" .. err .. ": " .. str)
-    end
-  )
-end
-
-local function tap_cb(when)
-  print("Button tapped")
-  setup()
-end
-
-local function hold_cb(when)
-  print("Button held")
-  print("Disconnecting from wifi")
-  wifi.sta.disconnect()
-end
-
-local wifi_status = wifi.sta.status()
-print("Wifi status:", wifi_status)
-
-if wifi_status == 5 then
-  start()
-else
-  setup()
+  if not setup_started then
+    print("Starting end user setup")
+    enduser_setup.start(
+      function()
+        print("Connected to wifi")
+        setup_started = false
+        animations:set_animation("none")
+      end,
+      function(err, str)
+        print("enduser_setup: Err #" .. err .. ": " .. str)
+        setup_started = false
+      end,
+      print
+    )
+    setup_started = true
+  else
+    print("End user setup is already running")
+  end
 end
 
 animations:init(8)
-ui:init(3, 2000, 300, hold_cb, tap_cb)
+button:init(3, 2000, 300,
+  function()
+    print("Button held, Disconnecting from wifi")
+    wifi.sta.disconnect()
+    animations:set_animation("no_network")
+  end,
+  function()
+    print("Button tapped, starting end user setup")
+    setup()
+  end
+)
+
+-- Run every minute
+-- If we're online, check bamboo
+-- If not, attempt to start the end user setup portal
+tmr.create():alarm((1 * 60 * 1000), tmr.ALARM_AUTO, function()
+  local wifi_status = wifi.sta.status()
+  print("Wifi status:", wifi_status)
+  if wifi_status == 5 then
+    print("Network up, check bambo...")
+    -- TODO Check bamboo and update LEDs accordingly
+  else
+    animations:set_animation("no_network")
+    setup()
+  end
+end)
+
