dcd917143a19baac42acc5fd7bf48f9d1256f397
Nathan Miller <nmiller014@gmail.com>
2017-03-30 21:50:37 -0500

    Added battery module

---
+++ battery.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+local function M.get_battery_mv(battery_config)
+  if battery_config.enabled then
+    -- convert to millivolts from raw ADC reading
+    local mv = (adc.read(0) * 1000) / battery_config.scaling_factor
+    -- Standard voltage divider math
+    local div = (battery_config.r2_ohms / (battery_config.r1_ohms + battery_config.r2_ohms))
+    local vbatt = mv / div
+    print("vbatt:", vbatt)
+
+    return vbatt
+  end
+
+  return nil, "Battery measurement disabled"
+end
+
+return M
