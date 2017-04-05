6b19465efe7fd269a88b30230e407918f82caf68
Nathan Miller <nmiller014@gmail.com>
2017-04-05 14:06:17 -0500

    Fixed bug in battery.lua and updated print

--- battery.lua
+++ battery.lua
@@ -1,13 +1,13 @@
 local M = {}
 
-local function M.get_battery_mv(battery_config)
+function M.get_battery_mv(battery_config)
   if battery_config.enabled then
     -- convert to millivolts from raw ADC reading
     local mv = (adc.read(0) * 1000) / battery_config.scaling_factor
     -- Standard voltage divider math
     local div = (battery_config.r2_ohms / (battery_config.r1_ohms + battery_config.r2_ohms))
     local vbatt = mv / div
-    print("vbatt:", vbatt)
+    print("vbatt (mv):", vbatt)
 
     return vbatt
   end
